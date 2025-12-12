6478a0f4e393994719c8b1e9ee42e32e3bfb5889
puntanosl <crismaescu@gmail.com>
2025-12-12 01:40:15 -0300

    Fix Firebase storageBucket

--- js/firebase.js
+++ js/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDnbovgIl9X0jkAUWSgoDFt_Yk03SwHjYE",
     authDomain: "biblioteca-1621c.firebaseapp.com",
     projectId: "biblioteca-1621c",
-    storageBucket: "biblioteca-1621c.firebasestorage.app",
+    storageBucket: "biblioteca-1621c.appspot.com", // ✅ ESTA ES LA LÍNEA CORREGIDA
     messagingSenderId: "703692855064",
     appId: "1:703692855064:web:1a01f02a3f42d2625eb06d",
     databaseURL: "https://biblioteca-1621c-default-rtdb.firebaseio.com"
